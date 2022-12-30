0b4483f4acb979ff2503eb8aff69b9cf51f26cda
shuncoder <vinhgatapkich@gmail.com>
2022-12-30 10:38:52 +0700

    Shun

---
+++ ShunCoder.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ShunCoder {
+	public static int factorial(int n) {
+		if (n == 1)
+			return 1;
+		return n * factorial(n - 1);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.print(factorial(n));
+		sc.close();
+	}
+}
